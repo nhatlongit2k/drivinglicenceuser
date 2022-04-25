74fa9576f534b7c91735bc8e4d02d0e453751c0e
LongTienSinh <nhatlongit2k@gmail.com>
2022-04-25 07:34:29 +0700

    first commit

---
+++ app/src/main/java/com/example/drivinglicenceuser/ui/activity/DoExamActivity.kt
@@ -0,0 +1,12 @@
+package com.example.drivinglicenceuser.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.drivinglicenceuser.R
+
+class DoExamActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_do_exam)
+    }
+}
